d04d96ffd4617306abc515118bc9960cba9a3aca
rdnanuar <rdjanuar008@gmail.com>
2020-11-09 16:47:35 +0700

    New File 1

--- latihan01.py
+++ latihan01.py
@@ -2,3 +2,36 @@ list=[1, 2 ,3, 4, 5, 6, 7]
 
 for x in list:
     print(x)
+
+
+def tambah(a, b):
+    return a + b
+
+def kurang(a, b):
+    return a - b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    return a / b
+
+print('+')
+print('-')
+print('*')
+print('/')
+pilih=input("Masukan Nilai Operasi:")
+A=int(input("Angka Pertama :"))
+B=int(input("Angka Kedua :"))
+if pilih == "+":
+    print(A,"+",B,'=',tambah(A,B))
+elif pilih == "-":
+    print(A,"-",B,'=',kurang(A,B))
+elif pilih == "*":
+    print(A,'*',B,'=',kali(A,B))
+elif pilih == "/":
+    print(A,'/',B,'=',bagi(A,B))
+
+else :
+    print("salah memasukan Operasi")
+    
