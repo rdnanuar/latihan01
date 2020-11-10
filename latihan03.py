8bfd0cf51edad8f457da9ce3a89459cfd631fa4c
rdnanuar <rdjanuar008@gmail.com>
2020-11-11 02:06:58 +0700

    New file 001

---
+++ latihan03.py
@@ -0,0 +1,39 @@
+a= 10
+b= 7
+hasil=a + b
+print(a,'+',b,'=',hasil)
+
+hasil=a - b
+print(a,'-',b,'=',hasil)
+
+hasil=a * b
+print(a,'*',b,'=',hasil)
+
+hasil=a / b
+print(a,'/',b,'=',hasil)
+
+hasil=a ** b
+print(a,'**',b,'=',hasil) # Pangkat eksponen
+
+hasil=a % b
+print(a,'%',b,'=',hasil) # sisa pembagaian modulus
+
+hasil=a // b
+print(a,'//',b,'=',hasil)# dibulatkan kebawah floor divison
+
+x= 3
+y= 2
+z= 4
+
+hasil= x ** y * z + x / y - y % z //x
+print(hasil)
+
+hasil= x ** y * (z + x) / y - y % z //x
+print(hasil)
+
+hasil= x + y * z
+print(hasil)
+
+
+hasil= (x + y) * z
+print(hasil)
